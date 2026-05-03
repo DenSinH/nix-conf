71bbae1d54e6a1b62b13de61502736f4d1803ce3
DenSinH <git@dennishilhorst.nl>
2026-05-03 17:59:36 +0200

    fix formatting

--- modules/features/dev/nixos.nix
+++ modules/features/dev/nixos.nix
@@ -10,13 +10,15 @@ let
   cfg = config.features.${featureName};
 in
 {
-  config = lib.mkIf cfg.enable (lib.mkMerge [
-    (lib.mkIf cfg.docker {
-      virtualisation.docker.enable = true;
+  config = lib.mkIf cfg.enable (
+    lib.mkMerge [
+      (lib.mkIf cfg.docker {
+        virtualisation.docker.enable = true;
 
-      users.users.dennis = {
-        extraGroups = [ "docker" ];
-      };
-    })
-  ]);
+        users.users.dennis = {
+          extraGroups = [ "docker" ];
+        };
+      })
+    ]
+  );
 }
